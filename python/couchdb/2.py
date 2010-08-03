1d6d4e26b16d479c5b7269d34efb9bf8b30b4f6f
vinod (mercury) <vinod@linuxense.com>
2010-08-03 12:01:17 +0530

    python couchdb delete a database

---
+++ python/couchdb/2.py
@@ -0,0 +1,11 @@
+from couchdb.client import Server
+
+server = Server('http://localhost:5984')
+try:
+	db = server.create('animals')
+except:
+	db = server['animals']
+
+print db.name
+
+del server[db.name]
